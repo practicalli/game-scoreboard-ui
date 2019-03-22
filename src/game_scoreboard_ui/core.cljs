299bb30a89fd6628b921d5587c7d58d50ebb468b
John Stevenson <john@jr0cket.co.uk>
2019-03-22 12:28:45 +0000

    Documented code sections
    Added documentation in comments regarding the purpose of the project and
    namespace.
    
    Added comments to delimit each section of the namespace.  This makes it easier
    to get up to speed with the namespace and make changes with confidence.
    Distinguishing sections also makes it easier to know when to refactor sections
    into their own namespace (and an idea of what to call that namespace).

--- src/game_scoreboard_ui/core.cljs
+++ src/game_scoreboard_ui/core.cljs
@@ -1,23 +1,44 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; game-scoreboard-ui
+;;
+;; A figwheel-main project with reagent
+;;
+;; A Single Page App to show how to consume
+;; the Game Scoreboard API.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (ns ^:figwheel-hooks game-scoreboard-ui.core
   (:require
    [goog.dom :as gdom]
    [reagent.core :as reagent :refer [atom]]))
 
+;; Helper functions
+;;;;;;;;;;;;;;;;;;;
+
 (println "This text is printed from src/game_scoreboard_ui/core.cljs. Go ahead and edit it and see reloading in action.")
 
 (defn multiply [a b] (* a b))
 
-
+;; Data model
+;;;;;;;;;;;;;
 ;; define your app data so that it doesn't get over-written on reload
 (defonce app-state (atom {:text "Hello world!"}))
 
-(defn get-app-element []
-  (gdom/getElement "app"))
+
+;; Web page components
+;;;;;;;;;;;;;;;;;;;;;;
 
 (defn hello-world []
   [:div
    [:h1 (:text @app-state)]
-   [:h3 "Edit this in src/game_scoreboard_ui/core.cljs and watch it change!"]])
+   [:h3 "A simple component that also displays the data model"]])
+
+
+;; System related functions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn get-app-element []
+  (gdom/getElement "app"))
 
 (defn mount [el]
   (reagent/render-component [hello-world] el))
